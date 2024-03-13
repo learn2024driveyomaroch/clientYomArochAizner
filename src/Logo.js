b07a09ffb92009d17748abf4572b90b6a9d5df04
boker2024 <nnn@gmail.com>
2024-03-13 15:16:03 +0200

    iii

---
+++ src/Logo.js
@@ -0,0 +1,9 @@
+//2015 ecma script es6
+
+export let x = 5;
+export function f() {
+    alert("f")
+}
+function f2() {
+    alert("f2")
+}
--- src/index.js
+++ src/index.js
@@ -4,14 +4,45 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { f, x } from "./Logo"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const root = ReactDOM.createRoot(document.getElementById('root'));
+f();
+// let a=document.createElement("h1").ser
+// let a = React.createElement("h1", { id: "hh" }, "hello");
+// let b=React.createElement("ul",{id:"u1"},React.createElement("li",null,"בננה"),React.createElement())
+let myName = "zipi"
+
+let a = <ul id="u1" className='good'>
+  <li style={{ color: "blue" }}>בננה</li>
+  <li>מלפפון</li>
+  <li>{myName}</li>
+  <li>{Math.random() * 10}</li>
+  <li>{5 / 2}</li>
+  <li>{x}</li>
+</ul>
+let b = <><p>hello</p> <span>to you</span></>
+root.render(
+  a
+)
+  ;
+//java script extension
+//הרחבה של גאוה סקריפט
+//תוסף
+//בתוך דפי גאוה סקריפט ובתוך משתנים וכפרמטרים לפונקציותhtml כותבים קוד של 
+// שתפקידו לקחת קוד גי אס איקס ולהמיר אותו לקוד של פונקציות גאוה סקריפט שבונה כאלה תגיותbabelישנו מנוע של
+
+//כללים של גי אס איקס:
+//1. חייבת להיות תגית עוטפת אחת
+//2. אם הקוד יורד על יותר משורה אחת מומלץ לעטוף בסוגריים עגולים
+//וזה בגלל שגאוה סקריפט מוסיף כראות עיניו נקדות ופסיקים
+//3.className  אין להשץמש בשמות שמורים של גאוה סקריפט  לדוגמא:נשתמש ב
+//class במקום ב-
+//4.camelCaseתכונות של תגית נכתבות ב
+//לדוגמא: onClick className tabIndex
+//5. אם רוצים לגשת למשתנה של גאוה סקריפט או תוצאה של פונקציה או חישוב 
+//יש להשתמש בצומדיים (רק מתוך קוד גי אסא איקס)
+//6.מקבלת לתוכה אובייקט style מ התכונה 
 reportWebVitals();
+
+
